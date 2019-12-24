9b3550c12c2ad6c1e4c60e5b4e7359910b9c0f71
elizanikol <55412039+elizanikol@users.noreply.github.com>
2019-12-24 15:27:28 +0300

    Update ps_5.sh

--- lab5/ps_5.sh
+++ lab5/ps_5.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-if [ $1 == $2 ]
-then
-        echo "$3"
-fi
+ps -e -o ruid,euid,cmd | while read txt
+do
+        row=($txt)
+        if [ ${row[0]} != ${row[1]} ]; then
+                echo "${row[2]}"
+        fi
+done
